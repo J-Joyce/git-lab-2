920401e930649809d75f8039ca119e1062592789
J-Joyce <143567718+J-Joyce@users.noreply.github.com>
2023-09-12 16:41:54 -0400

    Add files via upload
    done with the C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <James Joyce>
+ *        Date: <9/5/2023>
+ * Description: A
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!!" << endl;
+    return 0;
+}// main
